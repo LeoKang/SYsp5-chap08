513d3c5889149636940ad43294b702dd2e7b197c
SYU <xeungjin@gmail.com>
2025-03-28 16:06:56 +0900

    ~ 205 page, memberDao 테스트

--- src/main/java/com/example/sp5_chap08/main/MainForMemberDao.java
+++ src/main/java/com/example/sp5_chap08/main/MainForMemberDao.java
@@ -5,6 +5,8 @@ import com.example.sp5_chap08.spring.MemberDao;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.example.sp5_chap08.config.AppCtx;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class MainForMemberDao {
@@ -15,7 +17,8 @@ public class MainForMemberDao {
         memberDao = ctx.getBean(MemberDao.class);
 
         selectAll();
-
+        updateMember();
+        insertMember();
         ctx.close();
     }
 
@@ -24,8 +27,30 @@ public class MainForMemberDao {
         int total = memberDao.count();
         System.out.println("전체 데이터: " + total);
         List<Member> members = memberDao.selectAll();
-        for(Member m : members) {
+        for (Member m : members) {
             System.out.println(m.getId() + ":" + m.getEmail() + ":" + m.getName());
         }
+    }
+
+    private static void updateMember() {
+        System.out.println("----- updateMember");
+        Member member = memberDao.selectByEmail("madvirus@madvirus.net");
+        String oldPw = member.getPassword();
+        String newPw = Double.toHexString(Math.random());
+        member.changePassword(oldPw, newPw);
+
+        memberDao.update(member);
+        System.out.println("암호 변경 : " + oldPw + " > " + newPw);
+    }
+
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMddHHmmss");
+
+    private static void insertMember() {
+        System.out.println("----- insertMember");
+
+        String prefix = formatter.format(LocalDateTime.now());
+        Member member = new Member(prefix + "@test.com", prefix, prefix, LocalDateTime.now());
+        memberDao.insert(member);
+        System.out.println(member.getId() + "데이터 추가");
     }
 }
