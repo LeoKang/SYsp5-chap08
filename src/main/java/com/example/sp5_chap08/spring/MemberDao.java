f4f6285658661651998efef77c1264b09467d095
SYU <xeungjin@gmail.com>
2025-03-28 11:25:42 +0900

    ~ 184 page

---
+++ src/main/java/com/example/sp5_chap08/spring/MemberDao.java
@@ -0,0 +1,21 @@
+package com.example.sp5_chap08.spring;
+
+import java.util.Collection;
+
+public class MemberDao {
+    public Member selectByEmail(String email) {
+        return null;
+    }
+
+    public void insert(Member member) {
+
+    }
+
+    public void update(Member member) {
+
+    }
+
+    public Collection<Member> selectAll() {
+        return null;
+    }
+}
